5f5ed30386ecf31e3d217768e6dae06cf212d57c
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-04-29 15:25:47 +0000

    script

--- DESAFIOS/hamburgueria/script.js
+++ DESAFIOS/hamburgueria/script.js
@@ -3,3 +3,9 @@ const janela = document.getElementById ("janela")
 botao.addEventListener("click", function() {
 janela.style.display="flex"
 })
+janela.addEventListener("click", function(event){
+    if (event.target===janela) {
+        
+    
+janela.style.display="none"}
+})
