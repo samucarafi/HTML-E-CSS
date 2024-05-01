c7a74a7730412d33315c67eb8f5c269d49a624c4
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-05-01 12:30:58 +0000

    script

--- DESAFIOS/hamburgueria/script.js
+++ DESAFIOS/hamburgueria/script.js
@@ -1,9 +1,10 @@
 const botao = document.getElementById ("botao")
 const janela = document.getElementById ("janela")
+const fechar = document.getElementById ("fechar")
 botao.addEventListener("click", function() {
 janela.style.display="flex"
 })
 janela.addEventListener("click", function(event){
-    if (event.target===janela) {
+    if (event.target===janela||event.target=== fechar) {
 janela.style.display="none"}
 })
